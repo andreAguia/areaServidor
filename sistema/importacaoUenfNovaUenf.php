7fa5f8c8e52c202f733e6dade557348f82da4740
Alat <alataguia@gmail.com>
2016-09-06 12:41:29 -0300

    Algumas Alterações

--- sistema/importacaoUenfNovaUenf.php
+++ sistema/importacaoUenfNovaUenf.php
@@ -149,7 +149,9 @@ if($acesso){
                     . "fen001.natural,nacion,dir,ger,"
                     . "categ_res,pais,ano_cheg,"
                     . "perfil,cpf,pis,dt_adm,dt_nasc,obs,dt_dem,"
-                    . "matr_est,causa_dem FROM fen001 where (vinc <> 9 or (vinc = 9 and perfil = 21)) and cpf is not null";
+                    . "matr_est,causa_dem,"
+                    . "cargo,funcao "
+                    . "FROM fen001 where (vinc <> 9 or (vinc = 9 and perfil = 21)) and cpf is not null";
 
             $conteudo = $uenf->select($select,true);
             
@@ -795,6 +797,268 @@ if($acesso){
                         }
                         }
                     }
+                    
+                    # Cargo e Função
+                    $cargo = $campo[40];
+                    $funcao = $campo[41];
+                    $NovoCargo = null;
+                    
+                    switch ($funcao){
+                            case 0:
+                                $NovoCargo = 0;
+                                break;
+                            case 1:         // Administrador de Empresas
+                                $NovoCargo = 6;
+                                break;
+                            case 2:         // Análise de Planejamento
+                                $NovoCargo = 74;
+                                break;
+                            case 3:         // Análise de Planejamento de Finanças
+                                $NovoCargo = 10;
+                                break;
+                            case 4:         // Análise de Planejamento de Patrimônio
+                                $NovoCargo = 13;
+                                break;
+                            case 5:         // Análise de Planejamento de Rec Humanos
+                                $NovoCargo = 14;
+                                break;
+                            case 6:         // Análise de Sistema e Suporte
+                                $NovoCargo = 8;
+                                break;
+                            case 7:         // Análise Térmica
+                                $NovoCargo = 102;
+                                break;
+                            case 8:         // Apoio Acadêmico
+                                $NovoCargo = 15;
+                                break;
+                            case 9:         // Arquiteto
+                                $NovoCargo = 15;
+                                break;
+                            case 10:        // Biblioteca
+                                $NovoCargo = 104;
+                                break;
+                            case 11:        // Biologia
+                                $NovoCargo = 16;
+                                break;
+                            case 12:        // Bioquimica
+                                $NovoCargo = 105;
+                                break;
+                            case 13:        // Ciências Contabeis
+                                $NovoCargo = 17;
+                                break;
+                            case 14:        // Ciências Econômicas
+                                $NovoCargo = 18;
+                                break;
+                            case 15:        // Comunicação Social
+                                $NovoCargo = 107;
+                                break;
+                            case 16:        // Educação
+                                $NovoCargo = 19;
+                                break;
+                            case 17:        // Educação Física
+                                $NovoCargo = 20;
+                                break;
+                            case 18:        // Engenharia Agronômica
+                                $NovoCargo = 21;
+                                break;
+                            case 19:        // Engenharia Civil
+                                $NovoCargo = 22;
+                                break;
+                            case 20:        // Engenharia de Alimentos
+                                $NovoCargo = 109;
+                                break;
+                            case 21:        // Engenharia Elétrica
+                                $NovoCargo = 25;
+                                break;
+                            case 22:        // Engenharia Eletrônica
+                                $NovoCargo = 110;
+                                break;
+                            case 23:        // Estatística
+                                $NovoCargo = 113;
+                                break;
+                            case 24:        // Fonoaudiologia
+                                $NovoCargo = 114;
+                                break;
+                            case 25:        // Geofísica
+                                $NovoCargo = 28;
+                                break;
+                            case 26:        // Geoinformática
+                                $NovoCargo = 29;
+                                break;
+                            case 27:        // Geologio e Geoquímica
+                                $NovoCargo = 30;
+                                break;
+                            case 28:        // História
+                                $NovoCargo = 117;
+                                break;
+                            case 29:        // Imunologia e Patologia Clínica
+                                $NovoCargo = 118;
+                                break;
+                            case 30:        // Jurídico
+                                $NovoCargo = 31;
+                                break;
+                            case 31:        // Matemática
+                                $NovoCargo = 120;
+                                break;
+                            case 32:        // Médicina Veterinária
+                                $NovoCargo = 121;
+                                break;
+                            case 33:        // Meio Ambiente
+                                $NovoCargo = 121;
+                                break;
+                            case 34:        // Meteorologia
+                                $NovoCargo = 33;
+                                break;
+                            case 35:        // Multimídia
+                                $NovoCargo = 122;
+                                break;
+                            case 36:        // Propaganda e Marketing
+                                $NovoCargo = 35;
+                                break;
+                            case 37:        // Quimica
+                                $NovoCargo = 124;
+                                break;
+                            case 38:        // Serviço Social
+                                $NovoCargo = 37;
+                                break;
+                            case 39:        // Zootecnia
+                                $NovoCargo = 38;
+                                break;
+                            case 40:        // Assistente Admin.
+                                $NovoCargo = 53;
+                                break;
+                            case 41:        // Assistente Admin. - Biblioteca
+                                $NovoCargo = 86;
+                                break;
+                            case 42:        // Assistente Admin. - Almoxarifado
+                                $NovoCargo = 54;
+                                break;
+                            case 43:        // Assistente Admin. - Desenv Humano
+                                $NovoCargo = 55;
+                                break;
+                            case 44:        // Assistente Admin. - Financeiro
+                                $NovoCargo = 56;
+                                break;
+                            case 45:        // Assistente Admin. - Material
+                                $NovoCargo = 57;
+                                break;
+                            case 46:        // Assistente Admin. - Pagamento
+                                $NovoCargo = 58;
+                                break;
+                            case 47:        // Assistente Admin. - Patrimonio
+                                $NovoCargo = 59;
+                                break;
+                            case 48:        // Assistente Admin. - Pessoal
+                                $NovoCargo = 60;
+                                break;
+                            case 49:        // Telefonia
+                                $NovoCargo = 61;
+                                break;
+                            case 50:        // Agrícola e Agropecuária
+                                $NovoCargo = 39;
+                                break;
+                            case 51:        // Biologia Celular
+                                $NovoCargo = 90;
+                                break;
+                            case 52:        // Bioquímica
+                                $NovoCargo = 91;
+                                break;
+                            case 53:        // Biotecnologia
+                                $NovoCargo = 92;
+                                break;
+                            case 54:        // Biotério
+                                $NovoCargo = 93;
+                                break;
+                            case 55:        // Desenho Técnico
+                                $NovoCargo = 40;
+                                break;
+                            case 56:        // Edificções
+                                $NovoCargo = 41;
+                                break;
+                            case 57:        // Editoração Eletrônica
+                                $NovoCargo = 42;
+                                break;
+                            case 58:        // Eletricidade
+                                $NovoCargo = 80;
+                                break;
+                            case 59:        // Eletronica
+                                $NovoCargo = 82;
+                                break;
+                            case 60:        // Enfermagem
+                                $NovoCargo = 89;
+                                break;
+                            case 61:        // Geofisica de Campo
+                                $NovoCargo = 94;
+                                break;
+                            case 62:        // Geomecânica
+                                $NovoCargo = 43;
+                                break;
+                            case 63:        // Hialotecnia
+                                $NovoCargo = 95;
+                                break;
+                            case 64:        // Imunologia e Patoligia Clinica
+                                $NovoCargo = 96;
+                                break;
+                            case 65:        // Informática
+                                $NovoCargo = 44;
+                                break;
+                            case 66:        // Instrumentação
+                                $NovoCargo = 97;
+                                break;
+                            case 67:        // Manutenção Elétrica
+                                $NovoCargo = 45;
+                                break;
+                            case 68:        // Mecânica
+                                $NovoCargo = 46;
+                                break;
+                            case 69:        // Metalografia
+                                $NovoCargo = 84;
+                                break;
+                            case 70:        // Meio Ambiente
+                                $NovoCargo = 98;
+                                break;
+                            case 71:        // Proj Hidrossanitários
+                                $NovoCargo = 48;
+                                break;
+                            case 72:        // Quimica
+                                $NovoCargo = 50;
+                                break;
+                            case 73:        // Auxiliar Tecnico Adminisrtativo
+                                $NovoCargo = 62;
+                                break;
+                            case 74:        // Motorista
+                                $NovoCargo = 63;
+                                break;
+                            case 75:        // Campo
+                                $NovoCargo = 65;
+                                break;
+                            case 76:        // Op Maq. Agrícolas
+                                $NovoCargo = 79;
+                                break;
+                            case 77:        // Adm
+                                $NovoCargo = 71;
+                                break;
+                            case 78:        // Limpeza
+                                $NovoCargo = 72;
+                                break;
+                            case 100:        // Prof Associado
+                                $NovoCargo = 128;
+                                break;
+                            case 101:        // Prof Titular
+                                $NovoCargo = 129;
+                                break;
+                            case 107:        // Analise Orçamento
+                                $NovoCargo = 101;
+                                break;
+                            case 108:        // Analise de Sistema
+                                $NovoCargo = 8;
+                                break;
+                            case 108:        // Analise de Sistema
+                                $NovoCargo = 8;
+                                break;
+                    }
+                    
+                    
                     
                     # tbservidor
                     $tabela = 'tbservidor';
