6773d23c6880f5df750b59738cb3710782cf2a11
andreAguia <alataguia@gmail.com>
2017-03-09 07:51:51 -0300

    melhorias na interface

--- _classes/interface.AreaServidor.php
+++ _classes/interface.AreaServidor.php
@@ -18,9 +18,15 @@
         # tag do cabeçalho
         echo '<header>';
         
+        # Verifica se a imagem é comemorativa
+        if(date("d.m") == "08.03"){
+        	$imagem = new Imagem(PASTA_FIGURAS.'uenf_mulher.jpg','Dia Internacional da Mulher',190,60);
+        }else{
+        	$imagem = new Imagem(PASTA_FIGURAS.'uenf.jpg','Uenf - Universidade do Norte Fluminense',190,60);
+        }
+		
         $cabec = new Div('center');
-        $cabec->abre();
-            $imagem = new Imagem(PASTA_FIGURAS.'uenf.jpg','Área do Servidor da Uenf',190,60);
+        $cabec->abre();            
             $imagem->show();
         $cabec->fecha();       
         
