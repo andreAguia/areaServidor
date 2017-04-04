362708b44609f6a3afc1531e7b8631a006be886a
Alat <alataguia@gmail.com>
2017-04-04 11:00:23 -0300

    Bug de troca de senha

--- sistema/trocarSenha.php
+++ sistema/trocarSenha.php
@@ -25,9 +25,6 @@ if($acesso)
 
     # Verifica a fase do programa
     $fase = get('fase');
-	
-	# Verifica se veio do login ou do sistema de pessoal
-	$origem = get('o');
 
     # Começa uma nova página
     $page = new Page();			
@@ -123,7 +120,7 @@ if($acesso)
         case "gravar" :
         $erro = 0;		  // flag de erro: 1 - tem erro; 0 - não tem	
         $msgErro = null; // repositório de mensagens de erro
-echo "--->".$origem;
+
         # pega as senhas digitadas
         $senha1 = post('senha1');
         $senha2 = post('senha2');
@@ -163,12 +160,7 @@ echo "--->".$origem;
             $data = date("Y-m-d H:i:s");
             $atividade = 'Alterou a própria senha';
             $intra->registraLog($idUsuario,$data,$atividade,'tbusuario',$idUsuario,2,$idServidor);
-            
-			if($origem == 'login'){
-				loadPage('grh.php');
-			}else{
-				loadPage('perfilUsuario.php');
-			}
+            loadPage("areaServidor");
             
         }else{
             alert($msgErro);
