5fbf55fa97808a662e4d9eb5cb74046d46cd707e
andreAguia <alataguia@gmail.com>
2017-05-23 00:16:38 -0300

    rotina de backup

--- sistema/backup.php
+++ sistema/backup.php
@@ -22,14 +22,8 @@ if($acesso)
 
     # Verifica a fase do programa
     $fase = get('fase');        # Qual a fase
-    $manual = get('manual');	# Usado na rotina de backup, verifica se o backup é automático ou manual
     $arquivoSql = get('arquivoSql');  # Usado para se exibir um arquivo sql
-    
-    # Ordem da tabela
-    $orderCampo = get('orderCampo');
-    $orderTipo = get('orderTipo');
-    
-    # Pega a data
+      
     $dataLista = retiraAspas(post('dataLista',get('dataLista',date("Y-m-d"))));
     
     # Começa uma nova página
@@ -61,7 +55,7 @@ if($acesso)
             $linkBotaoAut->set_accessKey('C');
 
             # Botão Fazer Backup Manual
-            $linkBotaoEditar = new Link("Backup Manual",'?fase=aguarde1&manual=TRUE');
+            $linkBotaoEditar = new Link("Backup Manual",'?fase=aguarde&manual=TRUE');
             $linkBotaoEditar->set_class('button');
             $linkBotaoEditar->set_title('Executa um backup manual agora');
             $linkBotaoEditar->set_accessKey('B');
@@ -96,7 +90,7 @@ if($acesso)
             
             # Divide a tela
             $grid2 = new Grid();
-            $grid2->abreColuna(5);
+            $grid2->abreColuna();
           
             # Troca o - por .
             $partesData = explode('-',$dataLista);
@@ -127,7 +121,7 @@ if($acesso)
                     $banco = $partesArquivo[6];
                     
                     $extensao = $partesArquivo[7];
-                    $arquivoRelatorio = substr($arquivo,0, -3)."txt";
+                    $arquivoRelatorio = substr($arquivo,0, -3)."sql";
 
                     if($extensao == 'sql'){
                         $dadosArquivo[] = array($dia,$hora,$tipo,$banco,$arquivoRelatorio);
@@ -136,28 +130,18 @@ if($acesso)
             }
             
             # Monta a tabela
-            $label = array("Dia","Hora","Tipo","Banco","Ver");
-            $width = array(20,15,15,40,10);
-            $align = array("center","center","center","left");
+            $label = array("Dia","Hora","Tipo","Banco");
+            #$width = array(20,15,15,40,10);
+            $align = array("center");
             $function = array (NULL);
             
             $tabela = new Tabela();
             $titulo = "Backups Efetuados em ".$dia;
 
             $tabela->set_conteudo($dadosArquivo);
-            $tabela->set_cabecalho($label,$width,$align);
+            $tabela->set_label($label);
+             $tabela->set_align($align);
             $tabela->set_titulo($titulo);
-            
-            # Botão para exibir o relatório do backup
-            $botao = new BotaoGrafico();
-            $botao->set_label('');
-            $botao->set_url('?dataLista='.$dataLista.'&arquivoSql=');
-            $botao->set_title('Visualiza arquido de backup');
-            $botao->set_image(PASTA_FIGURAS.'ver.png',20,20);
-    
-            # Coloca o objeto link na tabela			
-            $tabela->set_link(array("","","","",$botao));
-            $tabela->set_idCampo(4);
             
             if(count($dadosArquivo) == 0){
                 $callout = new Callout();
@@ -167,60 +151,12 @@ if($acesso)
             }else{
                 $tabela->show();
             }
-            
-            $grid2->fechaColuna();
-
-            # Coluna da documentação detalhada
-            $grid2->abreColuna(7);
-            
-                if(is_null($arquivoSql)){
-                    $callout = new Callout();
-                    $callout->abre();
-                        p('Nenhum arquivo selecionado !!','center');
-                    $callout->fecha();
-                }else{
-                    echo '<pre>';
-
-                    # Define o arquivo
-                    $arquivoExemplo = $pasta."/".$arquivoSql;
-
-                    # Exibe o nome do arquivo
-                    echo str_repeat("#", 80);
-                    br();
-                    echo '# Arquivo:'.$arquivoExemplo;
-                    br();       
-                    echo str_repeat("#", 80);
-                    br(2);
-
-                    # variável que conta o número da linha
-                    $numLinha = 1;
-
-                    # Verifica a existência do arquivo
-                    if(file_exists($arquivoExemplo)){
-                        $linesCodigo = file($arquivoExemplo);
-
-                        # Percorre o arquivo e guarda os dados em um array
-                        foreach ($linesCodigo as $linha) {
-                            $linha = htmlspecialchars($linha);
-
-                                # Exibe o número da linha
-                                echo "<span id='numLinhaCodigo'>".formataNumLinha($numLinha)."</span> ";
-
-                                # Exibe o código
-                                echo $linha;
-
-                                # Incrementa o ~umero da linha
-                                $numLinha++;
-                        }
-                    }
-                }
-            
-            
+                        
             $grid2->fechaColuna();
             $grid2->fechaGrid();
             break;
             
-        case "aguarde1" :
+        case "aguarde" :
             br(10);
             aguarde();
             br();
@@ -228,80 +164,29 @@ if($acesso)
             # Limita a tela
             $grid1 = new Grid("center");
             $grid1->abreColuna(5);
-                p("Efetuando backup do banco do GRH","center");
+                p("Efetuando backup ...","center");
             $grid1->fechaColuna();
             $grid1->fechaGrid();
             
-            loadPage('?fase=grh&manual=TRUE');
+            loadPage('?fase=backup&manual=TRUE');
             break;    
         
-        case "grh":
-            $db = new Backup('grh',$manual,$intra->get_usuario($idUsuario));
-            $backup = $db->backup();
-            
-            if(!$backup['error']){
-                // If there isn't errors, show the content
-                // The backup will be at $var['msg']
-                // You can do everything you want to. Like save in a file.
-                // $fp = fopen('file.sql', 'a+');fwrite($fp, $backup['msg']);fclose($fp);
-                echo nl2br($backup['msg']);
-            } else {
-                echo 'An error has ocurred.';
+        case "backup":
+            $pastaBackup = $intra->get_variavel('pastaBackup');
+            $pasta = "../$pastaBackup/".date('Y.m.d');
+            if(!file_exists($pasta)){
+                    mkdir($pasta);
             }
+                       
+            $nomeArquivo = date('Y.m.d.H.i');
+            exec("backupGrh.bat ".date('Y.m.d')."/$nomeArquivo.M.grh");
+            exec("backupAreaServidor.bat ".date('Y.m.d')."/$nomeArquivo.M.areaServidor");
             
             # Escreve o log
             $data = date("Y-m-d H:i:s");
-            $atividade = "Efetuou o Backup Manual do Banco grh";
+            $atividade = "Efetuou o Backup Manual";
             $intra->registraLog($idUsuario,$data,$atividade,NULL,NULL,6,NULL);
-            
-            # Emite email se for manual
-            if($manual){
-                $db->emiteEmail();
-            }
-            
-            loadPage('?fase=aguarde2&manual='.$manual);
-            break;
-            
-        case "aguarde2" :
-            br(10);
-            aguarde();
-            br();
-            
-            # Limita a tela
-            $grid1 = new Grid("center");
-            $grid1->abreColuna(5);
-                p("Efetuando backup do banco da Área do Servidor","center");
-            $grid1->fechaColuna();
-            $grid1->fechaGrid();
-            
-            loadPage('?fase=areaservidor&manual=TRUE');
-            break;        
-            
-        case "areaservidor":
-            $db = new Backup('areaservidor',$manual,$intra->get_usuario($idUsuario));
-            $backup = $db->backup();
-            
-            if(!$backup['error']){
-                // If there isn't errors, show the content
-                // The backup will be at $var['msg']
-                // You can do everything you want to. Like save in a file.
-                // $fp = fopen('file.sql', 'a+');fwrite($fp, $backup['msg']);fclose($fp);
-                echo nl2br($backup['msg']);
-            } else {
-                echo 'An error has ocurred.';
-            }
-            
-            # Escreve o log
-            $data = date("Y-m-d H:i:s");
-            $atividade = "Efetuou o Backup Manual do Banco areaservidor";
-            $intra->registraLog($idUsuario,$data,$atividade,NULL,NULL,6,NULL);
-            
-            # Emite email se for manual
-            if($manual){
-                $db->emiteEmail();
-            }
-            
-            # Emite 
+                                   
             loadPage('?');
             break;
     }
---
+++ sistema/backupGrh.bat
@@ -0,0 +1,2 @@
+cd C:\xampp\mysql\bin
+mysqldump -u root -pDSvuEtwz6h9HfLCF grh > C:\www\areaServidor\_backup\%1.sql
