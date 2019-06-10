60ea9c3d32b32d02c3d30e4c16cc1969a91603f0
andre <andre@alat-pc>
2019-06-10 00:52:54 -0300

    Menu Principal Bug da rotina de estatística Menu da área do Servidor

--- _classes/interface.AreaServidor.php
+++ _classes/interface.AreaServidor.php
@@ -143,5 +143,246 @@
         $grid->fechaGrid();        
     }
 
+    ##########################################################
+    
+    /**
+    * Método menuPrincipal
+    * Exibe oo rodapé
+    * 
+    * @param    string $idUsuario -> Usuário logado
+    */
+    public static function menuPrincipal($idUsuario) {
+       
+        # Cria Grid
+        $grid = new Grid();
+        
+        # Primeira Coluna
+        $grid->abreColuna(12,6,4);
+        
+        # Módulos
+        self::moduloSistemas($idUsuario);
+                
+        $grid->fechaColuna();
+        
+        # Terceira Coluna
+        $grid->abreColuna(12,6,4);
+        
+        # Módulos
+        self::moduloServidoresUniversidade($idUsuario);
+                
+        $grid->fechaColuna();
+        
+        # Terceira Coluna
+        $grid->abreColuna(12,6,4);
+
+        # Módulos
+        self::moduloSobreServidor();
+        
+        $grid->fechaColuna();
+        $grid->fechaGrid();
+        
+        
+    }
+        
+    ###########################################################
+    
+    /**
+     * Método moduloTabelaAuxiliares
+     * 
+     * Exibe o menu de Legislação
+     */
+    
+    private static function moduloSistemas($idUsuario){
+        
+        $painel = new Callout();
+        $painel->abre();
+        
+        # Título
+        titulo('Sistemas');
+        $tamanhoImage = 64;
+        br();
+        
+        # Inicia o menu
+        $menu = new MenuGrafico();
+
+        # Sistema de Pessoal
+        $botao = new BotaoGrafico();
+        $botao->set_label('Sistema de Pessoal');
+        $botao->set_url('../../grh/grhSistema/grh.php');
+        $botao->set_imagem(PASTA_FIGURAS.'sistemaPessoal.png',$tamanhoImage,$tamanhoImage);
+        $botao->set_title('Acessa o Sistema de Pessoal');
+        $botao->set_accesskey('P');
+        $menu->add_item($botao);
+
+
+        # Sistema de Processos
+        if(Verifica::acesso($idUsuario,5)){                    
+            $botao = new BotaoGrafico();
+            $botao->set_label('Sistema de Processos');
+            $botao->set_url('processo.php');
+            $botao->set_imagem(PASTA_FIGURAS.'processo.png',$tamanhoImage,$tamanhoImage);
+            $botao->set_title('Sistema de controle de processos');
+            $botao->set_target("_blank");
+            $menu->add_item($botao);
+        }
+
+        # Controle de pastas Digitalizadas
+        if(Verifica::acesso($idUsuario,4)){
+            $botao = new BotaoGrafico();
+            $botao->set_label('Pastas Digitalizadas');
+            $botao->set_url('?fase=pastasDigitalizadas');
+            #$botao->set_url('pastaDigitalizada.php');
+            $botao->set_imagem(PASTA_FIGURAS.'pasta.png',$tamanhoImage,$tamanhoImage);
+            $botao->set_title('Sistema de controle de pastas digitalizadas');
+            $botao->set_accesskey('D');
+            $menu->add_item($botao);
+        }
+
+        $menu->show();
+        $painel->fecha();
+    }
+        
+    ###########################################################
+    
+    /**
+     * Método moduloTabelaAuxiliares
+     * 
+     * Exibe o menu de Legislação
+     */
+    
+    private static function moduloServidoresUniversidade($idUsuario){
+        
+        $painel = new Callout();
+        $painel->abre();
+        
+        titulo('Servidores da Universidade');
+        $tamanhoImage = 64;
+        br();
+            
+        if(Verifica::acesso($idUsuario,3)){
+            $menu = new MenuGrafico(4);
+
+            $botao = new BotaoGrafico();
+            $botao->set_label('Geral');
+            $botao->set_url('servidorGeral.php');
+            $botao->set_imagem(PASTA_FIGURAS.'admin.png',$tamanhoImage,$tamanhoImage);
+            $botao->set_title('Lista geral de servidores');
+            $menu->add_item($botao);
+        }else{
+            $menu = new MenuGrafico(3);
+        }
+
+        $botao = new BotaoGrafico();
+        $botao->set_label('por Lotação');
+        $botao->set_url('servidorLotacao.php');
+        $botao->set_imagem(PASTA_FIGURAS.'computador.png',$tamanhoImage,$tamanhoImage);
+        $botao->set_title('Lista de servidores por lotação');
+        $menu->add_item($botao);
+
+        $botao = new BotaoGrafico();
+        $botao->set_label('por Cargo Efetivo');
+        $botao->set_url('servidorCargo.php');
+        $botao->set_imagem(PASTA_FIGURAS.'cracha.png',$tamanhoImage,$tamanhoImage);
+        $botao->set_title('Lista de servidores por cargo efetivo');
+        $menu->add_item($botao);
+
+        $botao = new BotaoGrafico();
+        $botao->set_label('por Cargo em Comissão');
+        $botao->set_url('servidorCargoComissao.php');
+        $botao->set_imagem(PASTA_FIGURAS.'comissao.png',$tamanhoImage,$tamanhoImage);
+        $botao->set_title('Lista de servidores por cargo em comissão');
+        $menu->add_item($botao);
+        $menu->show();
+            
+        $painel->fecha();
+    }
+        
+    ###########################################################
+    
+    /**
+     * Método moduloTabelaAuxiliares
+     * 
+     * Exibe o menu de Legislação
+     */
+    
+    private static function moduloSobreServidor(){
+        
+        $painel = new Callout();
+        $painel->abre();
+        
+        titulo('Sobre o Servidor');
+        $tamanhoImage = 64;
+        br();
+
+        $menu = new MenuGrafico();
+
+        $botao = new BotaoGrafico();
+        $botao->set_label('Histórico de Licença');
+        $botao->set_url('?fase=historicoLicenca');
+        $botao->set_imagem(PASTA_FIGURAS.'licenca.jpg',$tamanhoImage,$tamanhoImage);
+        $botao->set_title('Exibe o seu histórico de licenças e afastamentos');
+        $menu->add_item($botao);
+
+        $botao = new BotaoGrafico();
+        $botao->set_label('Histórico de Férias');
+        $botao->set_url('?fase=historicoFerias');
+        $botao->set_imagem(PASTA_FIGURAS.'ferias.jpg',$tamanhoImage,$tamanhoImage);
+        $botao->set_title('Exibe o seu histórico de férias');
+        $menu->add_item($botao);
+
+        $botao = new BotaoGrafico();
+        $botao->set_label('Férias do seu Setor');
+        $botao->set_url('?fase=feriasSetor');
+        $botao->set_imagem(PASTA_FIGURAS.'feriasSetor.png',$tamanhoImage,$tamanhoImage);
+        $botao->set_title('Exibe as férias dos servidores do seu setor');
+        $menu->add_item($botao);
+
+        $menu->show();            
+        $painel->fecha();
+    }
+        
+    ###########################################################
+    
+    /**
+     * Método moduloTabelaAuxiliares
+     * 
+     * Exibe o menu de Legislação
+     */
+    
+    private static function modulolinksExternos(){
+        
+        $painel = new Callout();
+        $painel->abre();
+        
+        tituloTable('Sobre o Servidor');
+        $tamanhoImage = 64;
+
+        $menu = new MenuGrafico();
+
+        $botao = new BotaoGrafico();
+        $botao->set_label('Histórico de Licença');
+        $botao->set_url('?fase=historicoLicenca');
+        $botao->set_imagem(PASTA_FIGURAS.'licenca.jpg',$tamanhoImage,$tamanhoImage);
+        $botao->set_title('Exibe o seu histórico de licenças e afastamentos');
+        $menu->add_item($botao);
+
+        $botao = new BotaoGrafico();
+        $botao->set_label('Histórico de Férias');
+        $botao->set_url('?fase=historicoFerias');
+        $botao->set_imagem(PASTA_FIGURAS.'ferias.jpg',$tamanhoImage,$tamanhoImage);
+        $botao->set_title('Exibe o seu histórico de férias');
+        $menu->add_item($botao);
+
+        $botao = new BotaoGrafico();
+        $botao->set_label('Férias do seu Setor');
+        $botao->set_url('?fase=feriasSetor');
+        $botao->set_imagem(PASTA_FIGURAS.'feriasSetor.png',$tamanhoImage,$tamanhoImage);
+        $botao->set_title('Exibe as férias dos servidores do seu setor');
+        $menu->add_item($botao);
+
+        $menu->show();            
+        $painel->fecha();
+    }
+    
     ###########################################################
 }
