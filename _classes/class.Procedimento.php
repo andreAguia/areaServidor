8733360d2e3356e9d609cfd845370c44f216df69
alataguia@gmail.com <>
2019-08-01 08:57:02 -0300

    Várias alterações

--- _classes/class.Procedimento.php
+++ _classes/class.Procedimento.php
@@ -2,91 +2,91 @@
 class Procedimento{
  /**
   * Abriga as várias rotina do Sistema de Manual de Procedimentos
-  * 
+  *
   * @author André Águia (Alat) - alataguia@gmail.com
-  * 
+  *
   */
-    
+
    ##########################################################
-    
+
     public function menuPrincipal($idProcedimento = NULL, $idUsuario = NULL){
     /**
      * Exibe o menu de categoria.
-     * 
+     *
      * @syntax Procedimento::menuCategorias;
-     * 
+     *
      * @param $idCategoria    integer NULL o id da categoria a ser ressaltado no menu informando que está sendo editada.
      * @param $idProcedimento integer NULL o id do procedimento a ser ressaltado no menu informando que está sendo editada.
-     * 
-     */    
-   
+     *
+     */
+
         # Acessa o banco de dados
         $intra = new Intra();
-        
+
         # Monta o painel
         #$painel = new Callout();
         #$painel->abre();
-        
+
         titulo("Menu");
-        
+
         # Pega os procedimentos do menu Inicial: idPai = 0
         if(Verifica::acesso($idUsuario,1)){
             $select = 'SELECT idProcedimento,
                               titulo,
                               descricao
                          FROM tbprocedimento
-                        WHERE idPai = 0    
+                        WHERE idPai = 0
                   ORDER BY idPai,numOrdem';
         }else{
             $select = 'SELECT idProcedimento,
                               titulo,
                               descricao
                          FROM tbprocedimento
-                        WHERE idPai = 0  
-                          AND visibilidade = 1 
+                        WHERE idPai = 0
+                          AND visibilidade = 1
                   ORDER BY idPai,numOrdem';
         }
-        
+
         $dados = $intra->select($select);
         $numCategorias = $intra->count($select);
-            
+
         # Verifica se tem Categorias cadastradas
         if($numCategorias > 0){
-            
+
             # Inicia o menu
             $menu1 = new Menu("menuProcedimentos");
             #$menu1->add_item('titulo1','Menu','?fase=menuCaderno');
-        
-            # Percorre o array 
+
+            # Percorre o array
             foreach ($dados as $valor){
                 $texto = $valor[1];
 
                 $menu1->add_item('titulo','<b>'.$texto.'</b>','?fase=exibeProcedimento&idProcedimento='.$valor[0],$valor[2]);
-                
+
                 # Verifica se tem filhos
                 $filhos = $this->get_filhosProcedimento($valor[0], $idUsuario);
-                
+
                 if(!is_null($filhos > 0)){
                     foreach ($filhos as $valorFilhos){
-                        
+
                         if($idProcedimento == $valorFilhos[0]){
                             $menu1->add_item('link','<b>'.$valorFilhos[1].'</b>','?fase=exibeProcedimento&idProcedimento='.$valorFilhos[0],$valorFilhos[2]);
                         }else{
                             $menu1->add_item('link',$valorFilhos[1],'?fase=exibeProcedimento&idProcedimento='.$valorFilhos[0],$valorFilhos[2]);
                         }
-                        
+
                         # Verifica se tem netos
                         $netos = $this->get_filhosProcedimento($valorFilhos[0], $idUsuario);
-                        
+
                         if(!is_null($netos > 0)){
-                            
+
                             foreach ($netos as $valorNetos){
                                 if($idProcedimento == $valorNetos[0]){
-                                    $menu1->add_item('sublink',"- <strong>".$valorNetos[1].'</strong>','?fase=exibeProcedimento&idProcedimento='.$valorNetos[0],$valorNetos[2]);
+                                    $menu1->add_item('sublink',"<strong>".$valorNetos[1].'</strong>','?fase=exibeProcedimento&idProcedimento='.$valorNetos[0],$valorNetos[2]);
                                 }else{
-                                    $menu1->add_item('sublink',"- ".$valorNetos[1],'?fase=exibeProcedimento&idProcedimento='.$valorNetos[0],$valorNetos[2]);
+                                    $menu1->add_item('sublink',$valorNetos[1],'?fase=exibeProcedimento&idProcedimento='.$valorNetos[0],$valorNetos[2]);
                                 }
-                                
+
                             }
                         }
                     }
@@ -94,61 +94,61 @@ class Procedimento{
             }
             $menu1->show();
         }
-        
+
         # Fecha o painel
         #$painel->fecha();
     }
 
     ###########################################################
-    
+
     function get_dadosProcedimento($idProcedimento){
-        
+
     /**
      * Fornece todos os dados da categoria
      */
-        
+
         # Pega os dados
         $select="SELECT *
                    FROM tbprocedimento
                   WHERE idProcedimento = $idProcedimento";
-        
+
         $intra = new Intra();
         $dados = $intra->select($select,FALSE);
-        
+
         return $dados;
     }
-    
+
     ###########################################################
-    
+
     function get_filhosProcedimento($idProcedimento, $idUsuario = NULL){
-        
+
     /**
      * Fornece todos os dados da categoria
      */
-        
+
         # Pega os dados
         $select="SELECT *
                    FROM tbprocedimento
                   WHERE idPai = $idProcedimento";
-        
+
         if(!Verifica::acesso($idUsuario,1)){
             $select .= " AND visibilidade = 1";
         }
-        
+
         $intra = new Intra();
         $dados = $intra->select($select);
-        
+
         return $dados;
     }
-    
+
     ###########################################################
-    
+
     function exibeProcedimento($idProcedimento, $idUsuario = NULL){
-        
+
     /**
      * Fornece todos os dados da categoria
      */
-        
+
         # Pega os dados
         $dados = $this->get_dadosProcedimento($idProcedimento);
         $link = $dados["link"];
@@ -156,7 +156,7 @@ class Procedimento{
         $titulo = $dados['titulo'];
         $descricao = $dados['descricao'];
         $idPai = $dados['idPai'];
-        
+
         # Dados do Pai
         $dadosPai = $this->get_dadosProcedimento($idPai);
         $pai = $dadosPai['titulo'];
@@ -182,13 +182,12 @@ class Procedimento{
 
         # Exibe o titulo do pai (quando houver)
         if(!vazio($pai)){
-            p($pai,"procedimentoPai");
-            hr("procedimento");
+           p($pai,"procedimentoPai");
         }
-        
+
         p($titulo,"procedimentoTitulo");
         p($descricao,"procedimentoDescricao");
-        br();
+        hr("procedimento");
 
             # Div onde vai exibir o procedimento
             $div = new Div("divNota");
@@ -212,6 +211,6 @@ class Procedimento{
         # Fecha o painel
         $painel->fecha();
     }
-    
+
     ###########################################################
 }
