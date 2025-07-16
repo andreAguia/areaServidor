5ea483d7f1b02df935dfab6f28e82b71397b38b2
André Águia <alataguia@gmail.com>
2025-07-16 16:33:15 -0300

    melhoria no cadastro de serviços

--- _classes/class.Procedimento.php
+++ _classes/class.Procedimento.php
@@ -24,8 +24,7 @@ class Procedimento {
             [2, "Arquivo JPG"],
             [3, "Arquivo PDF"],
             [4, "Link"],
-            [5, "Rotina"],
-            [6, "Anexo Serviço"]
+            [5, "Rotina"]
         ];
     }
 
