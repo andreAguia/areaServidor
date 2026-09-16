ea7687a225cff02e0ad60d480d00b1e9904efe94
André Águia <alataguia@gmail.com>
2026-09-16 14:45:37 -0300

    varias coisas

--- _classes/class.Procedimento.php
+++ _classes/class.Procedimento.php
@@ -24,7 +24,7 @@ class Procedimento {
             [2, "Arquivo JPG"],
             [3, "Arquivo PDF"],
             [4, "Link"],
-            [5, "Rotina"]
+            [5, "Rotina"]         
         ];
     }
 
